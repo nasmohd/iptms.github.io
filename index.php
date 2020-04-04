925246cafb01e438a351535531ec9d808cdfa6cc
nasmohd40@gmail.com <nasmohd40@gmail.com>
2020-04-04 18:05:19 +0300

    a mis-spelt word made everything look ********

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         
             <div class="col">
             <nav class="navbar navbar-expand-lg" id="navTop">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSuppoertedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 
                 </button>
