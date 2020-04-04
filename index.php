8122ebe9ce2bbe72ae457d7b0c7c7d2f9ac4dc44
nasmohd40@gmail.com <nasmohd40@gmail.com>
2020-04-04 18:08:06 +0300

    added navbar-brand

--- index.php
+++ index.php
@@ -35,7 +35,9 @@
         <div class="row">
         
             <div class="col">
-            <nav class="navbar navbar-expand-lg" id="navTop">
+            <nav class="navbar navbar-expand-lg bg-dark" id="navTop">
+                <a class="navbar-brand" href="#" style="color:white;"> IPT Management System</a>
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 
