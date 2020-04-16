dda731aef65bb2e89c2326da7605bf7ea8243cb9
nasmohd40@gmail.com <nasmohd40@gmail.com>
2020-04-16 16:31:49 +0300

    studentcss style added to students page

--- phpIncludes/login.inc.php
+++ phpIncludes/login.inc.php
@@ -8,21 +8,27 @@ $pwd = $_POST ['pwd'];
 $sqlQuery1 = "SELECT RegNo, StudentPassword FROM student_info";
 
 $result = $conn -> query($sqlQuery1);
-$row = $result -> fetch_assoc();
+//$row = $result -> fetch_assoc();
 //echo $row['RegNo'];
 
-if (in_array($regNo, $row)){
-    //in_array() is a function which accepts a value to be tested as
-    //the first parameter and an array to be tested in second parameter
-    
-//    echo $row['StudentPassword']; //display the password
-    
-    
-    $url1 = "http://localhost/UNI_3rd_year/student/index.php";
-//    $url1 = "http://localhost/UNI_3rd_year/welcome.php";
-    header('Location: '.$url1);
-    exit();
+while ($row = $result -> fetch_assoc()){
+    echo $row['RegNo'];
 }
+
+
+
+//if (in_array($regNo, $row)){
+//    //in_array() is a function which accepts a value to be tested as
+//    //the first parameter and an array to be tested in second parameter
+//    
+////    echo $row['StudentPassword']; //display the password
+//    
+//    
+//    $url1 = "http://localhost/UNI_3rd_year/student/index.php";
+////    $url1 = "http://localhost/UNI_3rd_year/welcome.php";
+//    header('Location: '.$url1);
+//    exit();
+//}
 
 
 
