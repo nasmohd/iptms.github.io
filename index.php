ea2622a228653df32e6c912f7990f50a43c06448
nasmohd40@gmail.com <nasmohd40@gmail.com>
2020-04-17 21:47:35 +0300

    normal code

--- index.php
+++ index.php
@@ -157,22 +157,7 @@
 <!-- End of the container-fluid class -->
     
     
-<!--    Footer -->
-        <footer class="footer-black aa-footer" 
-            style="background-color: #306FA0; color: white; position: absolute; bottom: 0; width:100%; font-size:13px;">
-     
-    <div class="container py-2">
-        <div class="row text-center">
-
-
-          <div class="col-md-12">
-            <p class="mt-3"> &copy; 2020 - IPT Management System </p>
-          </div>
-        </div>
-      </div>
-    </footer>
-    
-    
+<!--    Footer -->    
 <?php 
     include "phpIncludes/footer.php";
 
