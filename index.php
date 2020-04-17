98e0df56236b76a135ec6f1ce1117b7ba56b6531
nasmohd40@gmail.com <nasmohd40@gmail.com>
2020-04-17 21:42:08 +0300

    added footer with pure HTML

--- index.php
+++ index.php
@@ -158,6 +158,21 @@
     
     
 <!--    Footer -->
+        <footer class="footer-black aa-footer" 
+            style="background-color: #306FA0; color: white; position: absolute; bottom: 0; width:100%; font-size:13px;">
+     
+    <div class="container py-2">
+        <div class="row text-center">
+
+
+          <div class="col-md-12">
+            <p class="mt-3"> &copy; 2020 - IPT Management System </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    
 <?php 
     include "phpIncludes/footer.php";
 
