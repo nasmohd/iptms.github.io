fb59046f9280cdd7d303d5f776af0f5a802fade9
nasmohd40@gmail.com <nasmohd40@gmail.com>
2020-05-30 16:11:00 +0300

    logbook

--- student/pages/daily_entry.php
+++ student/pages/daily_entry.php
@@ -8,43 +8,88 @@
     <div class="row mt-4">
         <div class="col-lg-10 col-12 ml-auto mr-auto">
             
-            <form method="post">
-                <div class="row" style="font-size:14px;">
-                    <div class="col-lg-2 col-4 mt-2">
+            <form method="post" action="../phpIncludes/daily_upload.inc.php">
+                <div class="row" style="font-size:14px;" id="dayinput">
+                    <div class="ml-5 col-lg-2 col-4">
                         <label for="exampleFormControlTextarea1">Week Number: </label>
-                        <input placeholder="Week 1 to 10" type="number" id="weekinput" class="form-control" style=" border: 2px solid #306FA0">
-                        
+                        <input placeholder="Week 1 to 10" name="weekNumber" type="text" id="weekinput" class="form-control" style=" border: 1px solid #306FA0">  
                     </div>
+                    
+                    <div class="col-lg-3 mr-auto"></div>
                     
                     <style>
-                        #weekinput{
+                        #dayinput div input{
                             font-size: 14px;
+                            border: 1px solid #306FA0
                         }
                         
-                    
+                        #dayinput div textarea{
+                            font-size: 14px;
+                            border: 1px solid #306FA0; resize:none;
+                        }
                     </style>
                     
-                    <div class="col-lg-3 col-6 mt-2 ml-auto mb-3">
-                        <label class="" for="exampleFormControlTextarea1">Select Date: </label>
-                        <input placeholder="DATE" type="text" class="form-control" style="border: 2px solid #306FA0" onfocus="(this.type='date')">
-                        
+                    <div class="ml-auto col-lg-8 col-6 mt-4">
+                        <label>FILL OUT DAILY ENTRIES BELOW: </label>
                     </div>
                     
-                    <div class="col-lg-12 col-12">
-                        <div class="form-group">
-<!--                    <label for="exampleFormControlTextarea1">Logbook Entry: </label>-->
-                            
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" style="border: 2px solid #306FA0; resize:none;"></textarea>
-                        </div>
+                    <div class="col-lg-3 mr-auto mt-4">
+                         <label class="" for="exampleFormControlTextarea1">Select Date: </label>
                     </div>
-                    
-                    <div class="col-lg-2 col-3 ml-auto mr-auto">
-                        <button type="submit" class="btn btn-primary"> Submit </button>
-                    </div>
-                
-                
-                </div>
 
+                    <div class="ml-auto col-lg-8">
+                        <textarea placeholder="Monday Entry" name="monEntry" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    
+                    <div class="col-lg-3 col-6 mr-auto">
+                        <input placeholder="DATE" type="text" name="monDate" class="form-control" onfocus="(this.type='date')">
+                    </div>
+                    
+                    <div class="ml-auto col-lg-8 mt-2">
+                        <textarea placeholder="Tuesday Entry" name="tueEntry" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    
+                    <div class="col-lg-3 col-6 mt-2 mr-auto">
+                        <input placeholder="DATE" type="text" name="tueDate" class="form-control" onfocus="(this.type='date')">
+                    </div>
+                    
+                    <div class="ml-auto col-lg-8 mt-2">
+                        <textarea placeholder="Wednesday Entry" name="wedEntry" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    
+                    <div class="col-lg-3 col-6 mt-2 mr-auto">
+                        <input placeholder="DATE" type="text" name="wedDate" class="form-control" onfocus="(this.type='date')">
+                    </div>                    
+                    
+                    <div class="ml-auto col-lg-8 mt-2">
+                        <textarea placeholder="Thursday Entry" name="thurEntry" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    
+                    <div class="col-lg-3 col-6 mt-2 mr-auto">
+                        <input placeholder="DATE" type="text" name="thurDate" class="form-control" onfocus="(this.type='date')">
+                    </div>                    
+                    
+                    <div class="ml-auto col-lg-8 mt-2">
+                        <textarea placeholder="Friday Entry" name="friEntry" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    
+                    <div class="col-lg-3 col-6 mt-2 mr-auto">
+                        <input placeholder="DATE" type="text" name="friDate" class="form-control" onfocus="(this.type='date')">
+                    </div>
+                    
+                    <div class="ml-auto col-lg-8 mt-2">
+                        <textarea placeholder="Saturday Entry" name="satEntry" class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    </div>
+                    
+                    <div class="col-lg-3 col-6 mt-2 mr-auto">
+                        <input placeholder="DATE" type="text" name="satDate" class="form-control" onfocus="(this.type='date')">
+                    </div>
+                    
+                    <div class="col-lg-2 mt-3 ml-auto mr-auto">
+                        <button type="submit" class="btn btn-success" name="daily_upload">
+                        UPLOAD </button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>    
