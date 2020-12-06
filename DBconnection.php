4aefeff1a5253743c2e93c753a3676a05d738078
nasmohd <nasmohd40@gmail.com>
2020-12-06 05:03:55 +0300

    Update DBconnection.php

--- DBconnection.php
+++ DBconnection.php
@@ -1,6 +1,6 @@
 <?php
     
-    return $conn = mysqli_connect('localhost', 'bd64fd2efb274f', 'c36d191a', 'ipt_system');
+    return $conn = mysqli_connect('localhost', 'root', '', 'ipt_system');
 
 if (!$conn){
     die("Connection failed: ".mysqli_connect_error());
